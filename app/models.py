63bc971282839a5b993efcc86b297209d7f48e09
Nifemi Bamgbose <nifemibams@gmail.com>
2022-07-02 22:11:09 +0100

    something fishy

--- app/models.py
+++ app/models.py
@@ -1,6 +1,3 @@
-from distutils.command.upload import upload
-import re
-from turtle import update
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
